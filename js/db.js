4291056ebc5bca0aa03ebdf09cace71a66fc5f65
Ashwin <ashwinlaly.com>
2021-07-17 23:44:24 +0530

    Adding Firestor to project

---
+++ js/db.js
@@ -0,0 +1,19 @@
+var contactForm = document.querySelector(".add-contact form")
+var contactModal = document.querySelector("#create_modal")
+
+contactForm.addEventListener("submit", e => {
+    e.preventDefault();
+    var contact = {
+        name: contactForm.name.value,
+        phone: contactForm.phone.value
+    }
+
+    db.collection("contact-pwa").add(contact).then(() => {
+        contactForm.reset();
+        var instance = M.Modal.getInstance(contactModal);
+        instance.close();
+        contactForm.querySelector(".error").testComtent = "";
+    }).catch(error => {
+        contactForm.querySelector(".error").textContent = error;
+    });
+})
